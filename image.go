ff1c6f87ac80590c78b5458d0949334cc7150413
malashin <malashinanton@gmail.com>
2019-04-03 03:31:01 +1000

    Alpha-premultiply RGB values

--- image.go
+++ image.go
@@ -92,10 +92,10 @@ func (i *img) Bounds() image.Rectangle {
 func (i *img) At(x, y int) color.Color {
 	arrPsn := i.pitch*y + i.stride*x
 	d := readBits(i.buf[arrPsn:], i.h.pixelFormat.rgbBitCount)
-	r := uint8((d & i.h.pixelFormat.rBitMask) >> i.rBit)
-	g := uint8((d & i.h.pixelFormat.gBitMask) >> i.gBit)
-	b := uint8((d & i.h.pixelFormat.bBitMask) >> i.bBit)
 	a := uint8((d & i.h.pixelFormat.aBitMask) >> i.aBit)
+	r := uint8(((d & i.h.pixelFormat.rBitMask) >> i.rBit) * uint32(a) / 255)
+	g := uint8(((d & i.h.pixelFormat.gBitMask) >> i.gBit) * uint32(a) / 255)
+	b := uint8(((d & i.h.pixelFormat.bBitMask) >> i.bBit) * uint32(a) / 255)
 	return color.RGBA{r, g, b, a}
 }
 
